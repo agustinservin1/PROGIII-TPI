d3bce2a79661f2be6614ec843a30942e23db1ed2
Federico Ramos <federicojramos42@gmail.com>
2024-10-20 12:21:09 -0300

    Patient Repository

---
+++ Clinica-Utn/Infrastructure/Data/PatientRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class PatientRepository : BaseRepository<Patient>
+    {
+        private readonly ApplicationContext _repository;
+
+        public PatientRepository(ApplicationContext repository) : base(repository)
+        {
+            _repository = repository;
+        }
+        public IEnumerable<Patient> GetAllPatientWithAddress()
+        {
+            var list = _repository.Patients
+                                   .Include(a => a.Address)
+                                   .ToList();
+            return list;
+        }
+        public Patient? GetByIdIncludeAddress(int id)
+        {
+            var entity = _repository.Patients.Include(a => a.Address)
+                                             .FirstOrDefault(c => c.Id == id);
+            return entity;
+        }
+    }
+}
