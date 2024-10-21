c967e512202a4d1fa70c4d16a751ad163101e478
JoaquinGuerrero25 <joaquinguerrero256@gmail.com>
2024-10-21 13:59:50 -0300

    AppointmentDto

---
+++ Clinica-Utn/Application/Models/AppointmentDto.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class AppointmentDto
+    {
+        public int IdAppointment { get; set; }
+        public int DoctorId { get; set; }
+        public int? PatientId { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Office { get; set; } = string.Empty;
+
+        public static AppointmentDto CreateDto(Appointment appointment)
+        {
+            var newAppointmentDto = new AppointmentDto()
+            {
+                IdAppointment = appointment.Id,
+                DoctorId = appointment.DoctorId,
+                PatientId = appointment.PatientId,
+                Date = appointment.Date,
+                Time = appointment.Time
+            };
+            return newAppointmentDto;
+        }
+
+        public static IEnumerable<AppointmentDto> CreateList(IEnumerable<Appointment> list)
+        {
+            List<AppointmentDto> listDto = new List<AppointmentDto>();
+            foreach (Appointment a in list)
+            {
+                listDto.Add(CreateDto(a));
+            }
+            return listDto;
+        }
+    }
+}
