270964a374d0f4fe7dd14ca7619fa0976e8b241b
JoaquinGuerrero25 <joaquinguerrero256@gmail.com>
2024-10-21 14:02:20 -0300

    AppointmentCreateRequest

---
+++ Clinica-Utn/Application/Models/Request/AppointmentCreateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Request
+{
+    public class AppointmentCreateRequest
+    {
+        public int DoctorId { get; set; }
+        public int PatientId { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Office { get; set; } = string.Empty;
+    }
+}
